1df1e97628cab3e66e20a5798efa14bbecef6e27
Jude M Battista <jbattista20@my.whitworth.edu>
2020-01-11 19:36:19 -0800

    First XML build
    Uses System.Xml (XmlDocument)
    Need to convert to System.Xml.Linq (XElement)

---
+++ linqTest/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace linqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We will need implictly typed variables for the return values
+            // We need to know the type ahead of time!
+            // The compiler cannot, however
+            // var myLinqInt = 0;
+
+            CrewMember mal = new CrewMember(
+                    "Malcolm",
+                    "Reynolds",
+                    "Captain"
+                );
+
+            CrewMember zoe = new CrewMember(
+                    "Zoe",
+                    "Washburne",
+                    "First Mate"
+                );
+
+            CrewMember jayne = new CrewMember(
+                    "Jayne",
+                    "Cobb",
+                    "Public Relations"
+                );
+
+            CrewMember wash = new CrewMember(
+                    "Hoban",
+                    "Washburne",
+                    "Pilot"
+                );
+
+            CrewMember inara = new CrewMember(
+                    "Inara",
+                    "Serra",
+                    "Ambassador"
+                );
+
+            CrewMember book = new CrewMember(
+                    "Derrial",
+                    "Book",
+                    "Shepherd"
+                );
+
+            CrewMember kaylee = new CrewMember(
+                    "Kaywinnit",
+                    "Frye",
+                    "Engineer"
+                );
+
+            CrewMember simon = new CrewMember(
+                    "Simon",
+                    "Tam",
+                    "Doctor"
+                );
+
+            CrewMember river = new CrewMember(
+                    "River",
+                    "Tam",
+                    "Psyker"
+                );
+
+            List<CrewMember> roster = new List<CrewMember> {
+                mal, zoe, jayne, wash, inara, book, kaylee, simon, river
+            };
+
+            XmlRoster xmlRoster = new XmlRoster(roster);
+            xmlRoster.Print();
+
+            CrewMember yoSaffBridge = new CrewMember(
+                "Yolanda Saffron Bridget",
+                "Unknown",
+                "Hazard"
+                );
+
+            xmlRoster.AddCrew(yoSaffBridge);
+            xmlRoster.Print();
+
+            /*
+            XmlDocument serenity = xmlRoster.GetXmlRoster();
+
+            IEnumerable<CrewMember> tams = from crew in serenity.FirstChild.
+                                           select crew;
+            */
+            string filename = "serenity.xml";
+            xmlRoster.Write(filename);
+            XElement serenity = XElement.Parse(filename);
+        }
+    }
+}
---
+++ linqTest/XmlRoster.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace linqTest
+{
+    class XmlRoster
+    {
+        List<CrewMember> roster;
+        XmlDocument xmlRoster;
+        bool rosterUpToDate;
+
+        public XmlRoster() {
+            roster = new List<CrewMember>();
+        }
+
+        public XmlRoster(List<CrewMember> roster) {
+            this.roster = roster;
+        }
+
+        public XmlDocument GetXmlRoster() {
+            if (!rosterUpToDate)
+            {
+                GenerateXmlRoster();
+            } 
+            return xmlRoster;
+        }
+
+        public void AddCrew(CrewMember newCrew) {
+            rosterUpToDate = false;
+            roster.Add(newCrew);
+        }
+
+        public void AddCrew(List<CrewMember> newRoster) {
+            rosterUpToDate = false;
+            roster.AddRange(newRoster);
+        }
+
+        public void RemoveCrew(CrewMember exCrew) {
+            rosterUpToDate = false;
+            roster.Remove(exCrew);
+        }
+
+        public void RemoveCrew(List<CrewMember> exCrew)
+        {
+            rosterUpToDate = false;
+            foreach (CrewMember ex in exCrew) {
+                roster.Remove(ex);
+            }
+        }
+
+        public void GenerateXmlRoster() {
+            XmlDocument doc = new XmlDocument();
+            XmlNode rootNode = doc.CreateElement("crew");
+            doc.AppendChild(rootNode);
+
+            foreach (CrewMember crewMember in roster) {
+                XmlNode crewMemberNode = doc.CreateElement("crewMember");
+                XmlAttribute fname = doc.CreateAttribute("fname");
+                fname.Value = crewMember.Fname;
+                crewMemberNode.Attributes.Append(fname);
+                XmlAttribute lname = doc.CreateAttribute("lname");
+                lname.Value = crewMember.Lname;
+                crewMemberNode.Attributes.Append(lname);
+                XmlAttribute position = doc.CreateAttribute("position");
+                position.Value = crewMember.Position;
+                crewMemberNode.Attributes.Append(position);
+                XmlAttribute rank = doc.CreateAttribute("rank");
+                rank.Value = crewMember.Rank;
+                crewMemberNode.Attributes.Append(rank);
+                rootNode.AppendChild(crewMemberNode);
+            }
+
+            xmlRoster = doc;
+            rosterUpToDate = true;
+        }
+
+        public void Print()
+        {
+            string sep = new string('=', 35);
+            string title = "Current Crew";
+            string filler = new string('=', title.Length);
+            if (!rosterUpToDate)
+            {
+                GenerateXmlRoster();    
+            }
+            Console.WriteLine("{0}{1}{0}", sep, title);
+            foreach (XmlNode child in xmlRoster.ChildNodes[0].ChildNodes) {
+                Console.WriteLine(child.OuterXml);
+            }
+            Console.WriteLine("{0}{1}{0}", sep, filler);
+        }
+
+        public void Write(string filename) {
+            xmlRoster.Save(filename);
+        }
+    }
+}
