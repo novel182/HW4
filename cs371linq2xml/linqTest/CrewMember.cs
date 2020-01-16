36e9a925f1566616fbd5ea0482ae687119d07d9a
npoudel21 <npoudel21@my.whitworth.edu>
2020-01-15 19:25:43 -0800

    first commit

---
+++ cs371linq2xml/linqTest/CrewMember.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace linqTest
+{
+    class CrewMember
+    {
+        public string Fname {
+            get;
+            set;
+        }
+        public string Lname {
+            get;
+            set;
+        }
+        public string Position {
+            get;
+            set;
+        }
+        public string Rank {
+            get;
+            set;
+        }
+
+        public CrewMember(string fname = "none", string lname= "none", string position= "none", string rank= "none") {
+            Fname = fname;
+            Lname = lname;
+            Position = position;
+            Rank = rank;
+        }
+    }
+}
